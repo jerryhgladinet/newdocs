9c74e4a722a5f33c025070a0a621ee567f4e2497
jerryhgladinet <43179553+jerryhgladinet@users.noreply.github.com>
2025-07-28 14:08:10 -0700

    Create conf.py

---
+++ admin/new_admin/conf.py
@@ -0,0 +1,25 @@
+# Configuration file for the Sphinx documentation builder.
+
+project = 'CentreStack Admin Guide'
+author = 'Gladinet'
+
+# Add any Sphinx extension module names here, as strings.
+extensions = [
+    'sphinx.ext.autosectionlabel',
+]
+
+# Paths that contain templates, relative to this directory.
+templates_path = ['_templates']
+
+# List of patterns, relative to source directory, that match files
+# and directories to ignore when looking for source files.
+exclude_patterns = []
+
+# The master toctree document.
+root_doc = 'index'
+
+# The theme to use for HTML and HTML Help pages.
+html_theme = 'alabaster'
+
+# Paths that contain custom static files, such as style sheets.
+html_static_path = ['_static']
